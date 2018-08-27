ee778a9f4fb9abd9860286ac06952274db6acf64
andreamcm <cor16076@uvg.edu.gt>
2018-08-27 17:13:19 -0600

    Avances

--- lista/src/App.js
+++ lista/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div className="Shop">
+        <h1>Shop</h1>
+        <div className="ShopList">
+        </div>
+       </div>
+       <div className="Add">
+        <div>
+          <h3>Name</h3>
+          <input className="nameInput"></input>
+          <input className="priceInput"></input>
+          <input className="quantityInput"></input>
+          <button>Add to shop</button>
+        </div>
+       </div>
+       <div className="Cart">
+        <h1>Client Cart</h1>
+       </div>
       </div>
     );
   }
